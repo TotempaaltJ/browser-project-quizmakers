2db32405056ecf8d579219f1b381de1ad33cc4bc
Bachar Daowd <bachar.daowd@gmail.com>
2021-05-31 14:35:16 +0200

    add previous btn

--- src/init/initializeQuiz.js
+++ src/init/initializeQuiz.js
@@ -4,9 +4,11 @@ import { QUESTION_CONTAINER_ID, QUIZ_CONTAINER_ID } from "../constants.js";
 import showCurrentQuestion from "../handlers/showCurrentQuestion.js";
 import createDOMElement from "../utils/createDOMElement.js";
 import getDOMElement from "../utils/getDOMElement.js";
+import createPreviousQuestionButtonElement from "../views/createPreviousQuestionButtonElement.js";
 import createNextQuestionButtonElement from "../views/createNextQuestionButtonElement.js";
 import { quizData } from '../data.js';
 import clearDOMElement from '../utils/clearDOMElement.js';
+
 
 const initializeQuiz = () => {
     quizData.currentQuestionIndex = 0;
@@ -22,16 +24,21 @@ const startButton = () => {
     startBtn.addEventListener('click', () => {
         clearDOMElement(userInterfaceContainer);
         setupQuizHTML();
-        showCurrentQuestion();
+        showCurrentQuestion();  
     })
     userInterfaceContainer.appendChild(startBtn)
 }
+
+
 const setupQuizHTML = () => {
     const userInterfaceContainer = getDOMElement('user-interface');
     const quizContainer = createDOMElement('div', { id: QUIZ_CONTAINER_ID });
     const questionContainer = createDOMElement('div', { id: QUESTION_CONTAINER_ID });
     quizContainer.appendChild(questionContainer);
 // add a previous button
+    const previousQuestionButton = createPreviousQuestionButtonElement();
+    quizContainer.appendChild(previousQuestionButton);
+    //Next button
     const nextQuestionButton = createNextQuestionButtonElement();
     quizContainer.appendChild(nextQuestionButton);
 
