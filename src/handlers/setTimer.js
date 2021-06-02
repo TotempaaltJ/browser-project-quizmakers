66097ee786cd535a6343230c7717e9f8e2c402a7
Bachar Daowd <72300050+bachar78@users.noreply.github.com>
2021-06-02 15:00:09 +0200

    lines indented with 4 spaces

--- src/handlers/setTimer.js
+++ src/handlers/setTimer.js
@@ -2,15 +2,15 @@
 const setTimeOut = () => {
     let sec = 60;
     const time = setInterval(myTimer, 1000);
-    
     function myTimer() {
+
     document.querySelector('#timer').innerHTML = `${sec} Second Left`;
     sec--;
     if (sec == -1) {
-        clearInterval(time);
-        alert(`Time Out`);
+    clearInterval(time);
+    alert(`Time Out`);
     }
-}
+  }
 }
 
 export default setTimeOut;
