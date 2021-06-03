a77bf3e21347858e624828650b9992bc901cbe9b
Bachar Daowd <72300050+bachar78@users.noreply.github.com>
2021-06-03 20:58:50 +0200

    fixing the indentation

--- src/handlers/handlePreviousQuestion.js
+++ src/handlers/handlePreviousQuestion.js
@@ -8,15 +8,16 @@ import {NEXT_QUESTION_BUTTON_ID } from '../constants.js';
 
 const handlePreviousQuestion = () => {
     
-     if (quizData.currentQuestionIndex === 9) {
-      const nextQuestionButton = getDOMElement(NEXT_QUESTION_BUTTON_ID);
-      nextQuestionButton.innerText = 'Next question';   
-    }
-  quizData.currentQuestionIndex = quizData.currentQuestionIndex - 1;
-  if (quizData.currentQuestionIndex < 1) {
-    quizData.currentQuestionIndex = 0;
-  }
-      showCurrentQuestion();
+ if (quizData.currentQuestionIndex === 9) {
+  const nextQuestionButton = getDOMElement(NEXT_QUESTION_BUTTON_ID);
+  nextQuestionButton.innerText = 'Next question';   
+}
+    
+quizData.currentQuestionIndex = quizData.currentQuestionIndex - 1;
+if (quizData.currentQuestionIndex < 1) {
+quizData.currentQuestionIndex = 0;
+}
+  showCurrentQuestion();
 }
 
 export default handlePreviousQuestion;
