e853c713bb39b408e5d743444b4f8bb82090f199
Bachar Daowd <72300050+bachar78@users.noreply.github.com>
2021-06-06 13:38:43 +0200

    Updated modifications

--- src/utils/selectAnswers.js
+++ src/utils/selectAnswers.js
@@ -2,7 +2,6 @@ import { quizData } from '../data.js';
 
 function selectAnswer(questionIndex, userAnswer) {
   quizData.questions[questionIndex].selected = userAnswer;
-  
 }
 
 export default selectAnswer;
